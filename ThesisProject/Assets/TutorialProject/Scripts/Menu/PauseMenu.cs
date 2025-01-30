1b89e90a4018144fee824943495339f88b8239db
AntonAnderss <ande15andersson@gmail.com>
2025-01-30 17:38:46 +0100

    Completed Tutorial 4.1 and started on 4.2 - Menu

---
+++ ThesisProject/Assets/TutorialProject/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PauseMenu : MonoBehaviour
+{
+    [Header("External")]
+    [SerializeField] private GameObject playerObject;
+    [SerializeField] private GameObject pauseMenuObject;
+    [SerializeField] private GameObject pauseStartMenuObject;
+    [SerializeField] private GameObject menuList;
+
+    [Header("Internal")]
+    [SerializeField] private bool isPaused = false;
+
+    private void Start()
+    {
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void OnPauseGame()
+    {
+
+    }
+    void PauseGame()
+    {
+        //Pauses the game
+        isPaused = true;
+        Time.timeScale = 0f;
+
+        //Disables the players input
+        playerObject.GetComponent<PlayerInput>().enabled = false;
+
+        //Enables mouse
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        //Enables pauseMenu
+        pauseMenuObject.gameObject.SetActive(true);
+
+    }
+    void UnPauseGame()
+    {
+
+    }
+    void ResetPauseUI()
+    {
+
+    }
+}
